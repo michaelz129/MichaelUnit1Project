aaebdcfba7b32e244d3af73a3b0245a8669892a8
BT_1N3_27 <sample@bths.edu>
2023-09-21 09:26:58 -0400

    commit 3

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -6,17 +6,22 @@ public class TipCalculator {
     double totalCost = 0;
     System.out.println("Welcome to Tip Calculator");
     System.out.print("How many people are in your group: ");
+    System.out.print();
     int people = scan.nextInt();
-    System.out.println("What is the tip percent?0-100: ");
-    double percent = scan.nextInt()/100;
+    System.out.print("What is the tip percent?0-100: ");
+    double percent = scan.nextInt();
     while(cost != -1){
         totalCost+=cost;
         System.out.print("Enter a cost in dollar and cent");
         cost=scan.nextDouble();
 
     }
-    System.out.println("Total bill before tip:" + totalCost);
-    System.out.println("Total percentage: " + percent*100 + "%'");
+    System.out.println("Total bill before tip: " + totalCost);
+    System.out.println("Total percentage: " + percent+ "%'");
+    double tp = totalCost*(percent/100);
+    System.out.print("Total tip:" + tp);
+    System.out.print("Total cost with tip: " + totalCost+tp);
+
 
     }
 }
