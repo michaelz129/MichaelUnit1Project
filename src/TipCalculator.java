941810e19a65dc4d9235c352443e309799b230c2
michaelz129 <michaelz129@nycstudents.net>
2023-09-29 00:01:22 -0400

    Commit 5

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,8 +1,12 @@
 import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.*;
 import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[]arg){
     Scanner scan = new Scanner(System.in);
+    List <String> items = new ArrayList<>();
+    String item = "--------------------------------------------";
     double cost = 0 ;
     double totalCost = 0;
     System.out.println("Welcome to Tip Calculator");
@@ -11,10 +15,24 @@ public class TipCalculator {
     int people = scan.nextInt();
     System.out.print("What is the tip percent?0-100: ");
     double percent = scan.nextInt();
+    System.out.print("Enter a cost in dollar and cent:");
+    cost=scan.nextDouble();
+    totalCost+=cost;
+    scan.nextLine();
+
+
+
     while(cost != -1){
-        totalCost+=cost;
+        System.out.print("Enter the item: ");
+        item = scan.nextLine();
+        items.add(item);
         System.out.print("Enter a cost in dollar and cent:");
         cost=scan.nextDouble();
+        totalCost+=cost;
+        scan.nextLine();
+
+
+
 
     }
     System.out.println("Total bill before tip: " + String.format("%.2f",totalCost));
@@ -26,6 +44,15 @@ public class TipCalculator {
     System.out.println("Per person cost before tp: " + ppcbt);
     System.out.println("Tip per person: " + tp/people);
     System.out.print("Total cost per person: " + String.format("%.2f",((totalCost+tp)/people)));
+
+    System.out.print("Items ordered:");
+    int itemsLen = items.toArray().length;
+    int i = 0;
+    for (i < itemsLen){
+        system.out.println(items(i));
+    }
+
+
 
 
     }
